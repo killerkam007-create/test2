60cc8934611903cccf84365ec23af2efbbba215f
kameshwar0607 <kameshwarsingh9402@gmail.com>
2025-10-10 18:01:12 +0530

    added new line of code as columns

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-# from pysaprk.sql import SparkSession
-# print("test 2 iniciated")
-# spark=SparkSession.builder.appName("TestApp").getOrCreate()
-# print("test 2 completed")
-import kagglehub
+from pyspark.sql import SparkSession
+print("test 2 iniciated")
+spark=SparkSession.builder.appName("TestApp").getOrCreate()
+print("test 2 completed")
+df = spark.read.csv(
+    "C:\\Users\\SURAJ\\OneDrive\\Desktop\\pro\\test2\\cities.csv",
+    header=True,
+    inferSchema=True
+)
+# df.show()
+df.printSchema()
+df.columns
+df.df["City"].show()
 
-# Download latest version
-path = kagglehub.dataset_download("tariqbashir/cities-of-india-with-pin-codes")
-
-print("Path to dataset files:", path)
+spark.stop()
