7128eaa591dc15a0a1a8856118546fc4396a7e5a
kameshwar0607 <kameshwarsingh9402@gmail.com>
2025-10-10 17:20:01 +0530

    addes location data

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from pysaprk.sql import SparkSession
-print("test 2 iniciated")
-spark=SparkSession.builder.appName("TestApp").getOrCreate()
-print("test 2 completed")
+# from pysaprk.sql import SparkSession
+# print("test 2 iniciated")
+# spark=SparkSession.builder.appName("TestApp").getOrCreate()
+# print("test 2 completed")
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("tariqbashir/cities-of-india-with-pin-codes")
+
+print("Path to dataset files:", path)
