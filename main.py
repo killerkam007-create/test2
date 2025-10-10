39a8b3319657ee206a95d0ddb3fe84f95c733eae
kameshwar0607 <kameshwarsingh9402@gmail.com>
2025-10-10 19:43:37 +0530

    main code updated

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pyspark.sql.functions import count
 from pyspark.sql import SparkSession
 from dict_cal_value import dict_cal
 
@@ -16,8 +17,9 @@ if __name__=="__main__":
     # df.show()
     df.printSchema()
     df.columns
-    df.select(df["city"],df["country"]).show()
-
+    assam_df = df.filter(df["State"] == "Assam").groupBy(df["District"]).agg(count(df["Pincode"]).alias("count"),sum(df["Pincode"]).alias("pin_sum"))
+    assam_df.wrrite.csv("C:\\Users\\SURAJ\\OneDrive\\Desktop\\pro\\test2\\Assam.csv",header=True)
+    df.write.partitionBy(df["State"]).csv("C:\\Users\\SURAJ\\OneDrive\\Desktop\\pro\\test2\\State_partitioned",header=True)
     spark.stop()
 
 
